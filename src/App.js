caaa6670153a5b0ce39320913695e968607887b5
Hitagi7 <20800091@usc.edu.ph>
2024-12-02 22:37:38 +0800

    preparing files

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sup</h1>
     </div>
   );
 }
-
-// jia the nig
-// marc likes bncs
 
 export default App;
---
+++ src/components/DisplayContainer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function DisplayContainer() {
+  return (
+    <div className="maincontainer">
+      <div className="outputcontainer"></div>
+      <div className="inputcontainer"></div>
+    </div>
+  );
+}
