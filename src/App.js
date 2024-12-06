c3adb14d931d2955de6fce4a551ea1819016f660
Hitagi7 <20800091@usc.edu.ph>
2024-12-06 18:15:52 +0800

    new file for the main container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import HeaderTitle from "./components/HeaderTitle";
 import "./App.css";
+import CalculatorContainer from "./components/CalculatorContainer";
 
 function App() {
   return (
     <div className="App">
       <HeaderTitle />
+      <div className="main-container">
+        <CalculatorContainer />
+      </div>
     </div>
   );
 }
---
+++ src/components/CalculatorContainer.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./CalculatorContainer.css";
+
+const CalculatorContainer = () => {
+  return <div className="outside-rectangle"></div>;
+};
+
+export default CalculatorContainer;
