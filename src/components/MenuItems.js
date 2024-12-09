02adabefe99dc843d81c646e253ad042040234d3
Mamon81 <jianna727@gmail.com>
2024-12-10 00:15:33 +0800

    Added datapoints

--- src/components/MenuItems.js
+++ src/components/MenuItems.js
@@ -16,3 +16,16 @@ export const MenuItems = [
   { title: "Tuguegarao", distance: 847 },
 ];
 
+export const DataPoints = [
+  { datadistance: 0, dataprice: 0 },
+  { datadistance: 117.57, dataprice: 2874.09 },
+  { datadistance: 130.95, dataprice: 2170.23 },
+  { datadistance: 153.37, dataprice: 3167.37 },
+  { datadistance: 163.25, dataprice: 1876.96 },
+  { datadistance: 175.60, dataprice: 3460.64 },
+  { datadistance: 194.70, dataprice: 2228.89 },
+  { datadistance: 201.19, dataprice: 2522.16 },
+  { datadistance: 204.81, dataprice: 3929.88 },
+  { datadistance: 222.64, dataprice: 1642.34 },
+  { datadistance: 228.50, dataprice: 3284.68 },
+];
